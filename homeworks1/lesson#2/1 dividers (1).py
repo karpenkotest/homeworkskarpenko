a370c6d5f60c4ea67662d7961112a015dbf13c26
karpenkotest <155569898+karpenkotest@users.noreply.github.com>
2024-01-28 16:44:13 +0200

    Delete homeworks1 directory

--- homeworks1/lesson#2/1 dividers (1).py
+++
@@ -1,9 +0,0 @@
-print("Please input integer number and i will show you all dividers :) ")
-number=int(input())
-range=int(number/2)#  dividers are less than half of the number or divider can be equal to the  half of the number
-divider_counter=1
-while (divider_counter<=range):
-    if not number%divider_counter:
-        print(divider_counter)
-    divider_counter+=1
-print(number)# adding number as own divider
